9b92ee2c70e8bf1a9222da49c37ad6ef90a909e8
Karl Burke <karl6658@yahoo.co.uk>
2024-06-18 11:43:18 +0100

    Added Link to Users and Users Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
+import Link from "next/link"
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to UFO Junction</h1>
-      <p>Keeping track of UFO stories and events since 2024!</p>
+      <p>Keeping track of UFO stories and events since 2024!</p>  
+      <br></br>    
+      <Link href="user">Users</Link>
     </div>
   );
 }
